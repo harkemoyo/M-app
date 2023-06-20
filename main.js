827b8ade6c77f42941e8168735b35f55f432f886
harkemoyo <markndeche91@gmail.com>
2023-06-20 10:17:28 +0300

    added li

--- main.js
+++ main.js
@@ -8,20 +8,26 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const db = getDatabase(app)
+
 // reference database
 const myListDb = ref(db, "Shopping")
 console.log(ready());
 
 const inputField = document.getElementById("input-field")
 const addCart = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
 
 addCart.addEventListener("click", fillInput)
 function fillInput() {
     let inputValue = inputField.value
     
     push(myListDb, inputValue)
+    // appended li
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${inputValue}`))
+    shoppingList.append(li)
     
     console.log(`${inputValue} added to database`)
 }
 
-// appending ul
+
